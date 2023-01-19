0edbb7e497fa570e492ce943f08e8fa7f77b22d3
chmini <papa00103@naver.com>
2023-01-19 18:52:13 +0900

    feat: connect login api

--- apps/frontend/src/components/Login/LoginForm.tsx
+++ apps/frontend/src/components/Login/LoginForm.tsx
@@ -1,32 +1,46 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { useSetRecoilState } from "recoil";
 import * as yup from "yup";
 
+import api from "@/api";
 import { Button } from "@/components/shared";
+import { accessTokenState } from "@/recoil/auth";
 import { sleep } from "@/utils/sleep";
 
+import type { LoginFormData } from "@/api/auth";
 import type { SubmitHandler } from "react-hook-form";
 
-interface LoginFormValue {
-  email: string;
-  password: string;
-}
-
 const schema = yup.object({
-  email: yup.string().required(),
-  password: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8),
 });
 
 export default function LoginForm() {
+  const setAccessToken = useSetRecoilState(accessTokenState);
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, errors },
-  } = useForm<LoginFormValue>({ resolver: yupResolver(schema) });
-  const onSubmit: SubmitHandler<LoginFormValue> = async (data) => {
-    // ANCHOR 로딩을 보기 위한 장치
-    await sleep(1000);
-    console.log(data);
+  } = useForm<LoginFormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit: SubmitHandler<LoginFormData> = async (formData) => {
+    try {
+      // 로딩 처리를 위한 임시 장치
+      await sleep(1000);
+
+      const { message, token } = await api.login(formData);
+      setAccessToken(token);
+      toast.success(message);
+    } catch (error) {
+      /**
+       * 로그인 실패처리
+       * 1. 토스트
+       * 2. 에러 메시지 렌더링
+       */
+    }
   };
 
   return (
