cd3188f45dcc3a32dddeca9fa8704a3b8572eeb2
chmini <papa00103@naver.com>
2023-01-26 23:26:26 +0900

    feat: style signup form and page, add error snackbar

--- apps/frontend/src/components/Signup/SignupForm.tsx
+++ apps/frontend/src/components/Signup/SignupForm.tsx
@@ -1,37 +1,34 @@
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Stack, TextField } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
+import { useSnackbar } from "notistack";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import * as yup from "yup";
 
 import api from "@/api";
-import { Button } from "@/components/shared";
+import { isErrorWithMessage } from "@/api/utils";
 import { useTokenActions } from "@/store/auth";
-import { sleep } from "@/utils/sleep";
 
 import type { SignupFormData } from "@/api/auth";
 import type { SubmitHandler } from "react-hook-form";
 
 interface SignupFormValues extends SignupFormData {
   passwordConfirmation: string;
-  signupError: string;
 }
 
-// TODO Add validation error message
 const schema = yup.object({
-  email: yup.string().required().email(),
-  password: yup.string().required().min(8),
+  email: yup.string().required("이메일을 입력해주세요").email("이메일 형식이 아닙니다다"),
+  password: yup.string().required("비밀번호를 입력해주세요").min(8, "비밀번호는 8자리 이상입니다"),
   passwordConfirmation: yup
     .string()
-    .required()
-    .oneOf([yup.ref("password")], "Your passwords do not match."),
+    .required("비밀번호를 다시 입력해주세요")
+    .oneOf([yup.ref("password")], "비밀번호가 일치하지 않습니다"),
 });
 
 export default function SignupForm() {
   const { setToken } = useTokenActions();
 
-  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const {
     handleSubmit,
@@ -42,40 +39,56 @@ export default function SignupForm() {
   const signupMutation = useMutation((formData: SignupFormData) => api.signup(formData), {
     onSuccess: ({ message, token }) => {
       setToken(token);
-      navigate("/", { replace: true });
-      toast.success(message);
+      enqueueSnackbar(message, { variant: "success" });
     },
     onError: (error) => {
-      // TODO 에러 처리
-      console.log(error);
+      if (isErrorWithMessage(error)) {
+        enqueueSnackbar(error.message, { variant: "error" });
+      }
     },
   });
 
-  const onSubmit: SubmitHandler<SignupFormValues> = async (formValues) => {
+  const onSubmit: SubmitHandler<SignupFormValues> = (formValues) => {
     const { email, password } = formValues;
-    // 로딩 처리를 위한 임시 장치
-    await sleep(1000);
     signupMutation.mutate({ email, password });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email">
-        이메일
-        <input id="email" placeholder="wanted@email.com" type="text" {...register("email")} />
-      </label>
-      {errors.email?.message}
-      <label htmlFor="password">
-        비밀번호
-        <input id="password" type="password" {...register("password")} />
-      </label>
-      {errors.password?.message}
-      <label htmlFor="passwordConfirmation">
-        비밀번호 확인
-        <input id="passwordConfirmation" type="password" {...register("passwordConfirmation")} />
-      </label>
-      {errors.passwordConfirmation?.message}
-      <Button type="submit">회원가입</Button>
+      <Stack spacing={4}>
+        <Stack spacing={2}>
+          <TextField
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            id="email"
+            label="이메일"
+            placeholder="papa@email.com"
+            variant="outlined"
+            {...register("email")}
+          />
+          <TextField
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            id="password"
+            label="비밀번호"
+            type="password"
+            variant="outlined"
+            {...register("password")}
+          />
+          <TextField
+            error={!!errors.passwordConfirmation}
+            helperText={errors.passwordConfirmation?.message}
+            id="passwordConfirmation"
+            label="비밀번호 확인"
+            type="password"
+            variant="outlined"
+            {...register("passwordConfirmation")}
+          />
+        </Stack>
+        <Button size="large" type="submit" variant="contained">
+          회원가입
+        </Button>
+      </Stack>
     </form>
   );
 }
--- apps/frontend/src/pages/Signup.tsx
+++ apps/frontend/src/pages/Signup.tsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom";
+import { Box, Container, Link, Stack } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 import SignupForm from "@/components/Signup/SignupForm";
 
 export default function SignupPage() {
   return (
-    <>
-      <SignupForm />
-      <Link to="/login">로그인 하러가기</Link>
-    </>
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          mt: 20,
+        }}
+      >
+        <Stack spacing={2}>
+          <SignupForm />
+          <Link component={RouterLink} textAlign="center" to="/login" variant="body2">
+            로그인 하러가기
+          </Link>
+        </Stack>
+      </Box>
+    </Container>
   );
 }
