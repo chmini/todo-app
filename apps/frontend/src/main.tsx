825152348ddfea67b529b7889490175b3c18f1a6
chmini <papa00103@naver.com>
2023-01-26 21:49:08 +0900

    feat: add theme, snackbar provider

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
 import ReactDOM from "react-dom/client";
+
+import theme from "@/styles/theme";
 
 import App from "./App";
 
@@ -10,7 +14,12 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider anchorOrigin={{ vertical: "top", horizontal: "center" }} autoHideDuration={3000}>
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
