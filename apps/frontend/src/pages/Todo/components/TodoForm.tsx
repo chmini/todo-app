4b1f5d3903b536fec91a453900d6084076066482
chmini <papa00103@naver.com>
2023-02-02 01:18:00 +0900

    fix: fix textfield focus error

--- apps/frontend/src/pages/Todo/components/TodoForm.tsx
+++ apps/frontend/src/pages/Todo/components/TodoForm.tsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Stack, TextField } from "@mui/material";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useSnackbar } from "notistack";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 
 import api from "@/api";
@@ -12,19 +14,28 @@ import type { TodoFormData } from "@/api/todo";
 import type { SubmitHandler } from "react-hook-form";
 
 const schema = yup.object({
-  title: yup.string().required(),
+  title: yup.string().required("Please enter Title"),
   content: yup.string(),
 });
 
 export default function TodoForm() {
   const { enqueueSnackbar } = useSnackbar();
-  const { register, handleSubmit } = useForm<TodoFormData>({ resolver: yupResolver(schema) });
+  const {
+    control,
+    handleSubmit,
+    reset,
+    setFocus,
+    formState: { errors },
+  } = useForm<TodoFormData>({
+    resolver: yupResolver(schema),
+  });
 
   const queryClient = useQueryClient();
   const { mutate } = useMutation((formData: TodoFormData) => api.createTodo(formData), {
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: ["todos"] });
-      // TODO 성공 스낵바(메시지, 투두 여는 버튼) 추가
+      enqueueSnackbar("Created Successfully", { variant: "success" });
+      reset();
     },
     onError: (error) => {
       if (isErrorWithMessage(error)) {
@@ -35,12 +46,49 @@ export default function TodoForm() {
 
   const onSubmit: SubmitHandler<TodoFormData> = (formData) => {
     mutate(formData);
+    setFocus("title");
+  };
+
+  const handleKeyDown = async (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      await handleSubmit(onSubmit)();
+    }
   };
 
   return (
     <Stack component="form" spacing={2} onSubmit={handleSubmit(onSubmit)}>
-      <TextField label="To do Name" variant="standard" {...register("title")} />
-      <TextField multiline label="Description" variant="standard" {...register("content")} />
+      <Controller
+        control={control}
+        defaultValue=""
+        name="title"
+        rules={{ required: true }}
+        render={({ field: { ref, ...field } }) => (
+          <TextField
+            error={!!errors.title}
+            helperText={errors.title?.message}
+            inputRef={ref}
+            label="Todo Title"
+            variant="standard"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        defaultValue=""
+        name="content"
+        render={({ field: { ref, ...field } }) => (
+          <TextField
+            multiline
+            inputRef={ref}
+            label="Description"
+            variant="standard"
+            onKeyDown={handleKeyDown}
+            {...field}
+          />
+        )}
+      />
       <Button size="large" type="submit">
         Add To Do
       </Button>
